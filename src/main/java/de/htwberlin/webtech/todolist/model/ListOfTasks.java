74ae378a8a4e3af98523803a782fd9e2ccde8f71
klsmann <kasimow.andrew@gmail.com>
2024-06-15 23:35:05 +0200

    fix ListOfTasks Entity and add Controller

--- src/main/java/de/htwberlin/webtech/todolist/model/ListOfTasks.java
+++ src/main/java/de/htwberlin/webtech/todolist/model/ListOfTasks.java
@@ -1,9 +1,17 @@
 package de.htwberlin.webtech.todolist.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class ListOfTasks {
 
     @Id
---
+++ src/main/java/de/htwberlin/webtech/todolist/web/ListOfTasksController.java
@@ -0,0 +1,15 @@
+package de.htwberlin.webtech.todolist.web;
+
+import de.htwberlin.webtech.todolist.service.ListOfTasksService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "https://todolistfrontend-qae0.onrender.com/")
+@RequestMapping(path = "/lists")
+public class ListOfTasksController {
+
+
+    private final ListOfTasksService listOfTasksService;
+}
