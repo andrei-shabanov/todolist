ad6c91211ab986e45cccdea2bf30a9a7fa372d19
klsmann <kasimow.andrew@gmail.com>
2024-06-15 23:11:42 +0200

    fix ListOfTasksRepo

--- src/main/java/de/htwberlin/webtech/todolist/persistence/ListOfTasksRepository.java
+++ src/main/java/de/htwberlin/webtech/todolist/persistence/ListOfTasksRepository.java
@@ -2,6 +2,8 @@ package de.htwberlin.webtech.todolist.persistence;
 
 import de.htwberlin.webtech.todolist.model.ListOfTasks;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ListOfTasksRepository extends CrudRepository<ListOfTasks, Long> {
 }
