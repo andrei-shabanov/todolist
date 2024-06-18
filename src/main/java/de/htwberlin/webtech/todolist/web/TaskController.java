ff653be291804d825b3be22c07b7931a4254e134
klsmann <kasimow.andrew@gmail.com>
2024-06-18 23:18:38 +0200

    no mappings (2)

--- src/main/java/de/htwberlin/webtech/todolist/web/TaskController.java
+++ src/main/java/de/htwberlin/webtech/todolist/web/TaskController.java
@@ -18,32 +18,31 @@ public class TaskController {
     // von repository aufrufen und edit methode aufrufen
     private final TaskService taskService;
 
-    @GetMapping(path = "/load",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Iterable<Task>> getTasks() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<Task>> getTask() {
         return ResponseEntity.ok(taskService.getTasks());
     }
 
-    @GetMapping("/{id}/get")
+    @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable("id") final Long id) {
         final Task found = taskService.getTask(id);
         return found != null ? ResponseEntity.ok(found) : ResponseEntity.notFound().build();
     }
 
-    @PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Task> addTask(@Valid @RequestBody Task body) {
         final Task createdTask = taskService.saveTask(body);
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 
-    // tasks/{id}update
-    @PutMapping(path = "/{id}/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Task> updateTask(@PathVariable("id") final long id, @RequestBody Task body) {
         final Task updatedTask = taskService.editTask(body);
         if (updatedTask == null) return ResponseEntity.notFound().build();
         else return ResponseEntity.ok(updatedTask);
     }
 
-    @DeleteMapping(path = "/{id}/delete")
+    @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable("id") final long id) {
         return taskService.removeTask(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
